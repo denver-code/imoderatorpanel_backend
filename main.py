77c36a65edb1f5fec48e18d92a72487058bc2370
Igor Savenko <csigorek@gmail.com>
2021-12-28 14:46:30 +0200

    refactor main.py

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import uvicorn
 import app.api.config_api as cp
 
 if __name__ == "__main__":
-    uvicorn.run("app.server:app", host=cp.get_value("host"), port=int(cp.get_value("port")), debug=cp.get_bool("debug"), reload=True)
+    uvicorn.run(
+            "app.server:app",
+            host="localhost",
+            port=8393,
+            debug=True,
+            reload=True
+            )
